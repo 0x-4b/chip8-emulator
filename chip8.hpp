292c790cee7b59a16f9e7ca86ef9deefb08ab4f2
0x-4b <kareem.m.dev@gmail.com>
2025-07-20 14:41:54 +0400

    Initialize class with public members and includers

---
+++ chip8.hpp
@@ -0,0 +1,27 @@
+#pragma once
+
+#include <iostream>
+#include <chrono>
+#include <cstdint>
+#include <fstream>
+#include <random>
+
+using u8 = uint8_t;
+using u16 = uint16_t;
+using u32 = uint32_t;
+
+class Chip8
+{
+    public:
+        u8 registers[16] {};
+        u8 memory[4096] {};
+        u16 index{};
+        u16 pc{};
+        u16 stack[16] {};
+        u8 sp{};
+        u8 delay_timer{};
+        u8 sound_timer{};
+        u8 keypad[16]{};
+        u32 video[64 * 32] {};
+        u16 opcode{};
+};
