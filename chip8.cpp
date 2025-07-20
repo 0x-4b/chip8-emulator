d2872b92a12c339c548baa425e2af335763e23f9
0x-4b <kareem.m.dev@gmail.com>
2025-07-20 16:39:49 +0400

    Finished 8xy4

--- chip8.cpp
+++ chip8.cpp
@@ -185,6 +185,20 @@ void Chip8::OP_8xy3()
 void Chip8::OP_8xy4()
 {
     // Set Vx = Vx + Vy, set VF = carry
+    u8 Vx = (opcode & 0x0F00u) >> 8;
+    u8 Vy = (opcode & 0x00F0u) >> 4;
+
+    u8 sum = registers[Vx] + registers[Vy];
+
+    if (sum > 255u)
+    {
+        registers[0xF] = 1;
+    }
+    else
+    {
+        registers[0xF] = 0;
+    }
+    registers[Vx] = sum & 0xFFu;
 }
 
 void Chip8::OP_8xy5()
