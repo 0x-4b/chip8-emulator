6480d0a94838050e8c276e63c345b98d9c736e0d
0x-4b <kareem.m.dev@gmail.com>
2025-07-20 15:50:17 +0400

    Decalred all instructions

--- chip8.cpp
+++ chip8.cpp
@@ -33,3 +33,178 @@ void Chip8::LoadRoam(char *const filename)
         delete[] buffer;
     }
 }
+
+void Chip8::OP_00E0()
+{
+    // Clear the display
+}
+
+void Chip8::OP_00EE()
+{
+    // Return from subroutine
+}
+
+void Chip8::OP_0nnn()
+{
+    // SYS addr (ignored in modern interpreters)
+}
+
+void Chip8::OP_1nnn()
+{
+    // Jump to location nnn
+}
+
+void Chip8::OP_2nnn()
+{
+    // Call subroutine at nnn
+}
+
+void Chip8::OP_3xkk()
+{
+    // Skip next instruction if Vx == kk
+}
+
+void Chip8::OP_4xkk()
+{
+    // Skip next instruction if Vx != kk
+}
+
+void Chip8::OP_5xy0()
+{
+    // Skip next instruction if Vx == Vy
+}
+
+void Chip8::OP_6xkk()
+{
+    // Set Vx = kk
+}
+
+void Chip8::OP_7xkk()
+{
+    // Set Vx = Vx + kk
+}
+
+void Chip8::OP_8xy0()
+{
+    // Set Vx = Vy
+}
+
+void Chip8::OP_8xy1()
+{
+    // Set Vx = Vx OR Vy
+}
+
+void Chip8::OP_8xy2()
+{
+    // Set Vx = Vx AND Vy
+}
+
+void Chip8::OP_8xy3()
+{
+    // Set Vx = Vx XOR Vy
+}
+
+void Chip8::OP_8xy4()
+{
+    // Set Vx = Vx + Vy, set VF = carry
+}
+
+void Chip8::OP_8xy5()
+{
+    // Set Vx = Vx - Vy, set VF = NOT borrow
+}
+
+void Chip8::OP_8xy6()
+{
+    // Set Vx = Vx >> 1, VF = least significant bit before shift
+}
+
+void Chip8::OP_8xy7()
+{
+    // Set Vx = Vy - Vx, set VF = NOT borrow
+}
+
+void Chip8::OP_8xyE()
+{
+    // Set Vx = Vx << 1, VF = most significant bit before shift
+}
+
+void Chip8::OP_9xy0()
+{
+    // Skip next instruction if Vx != Vy
+}
+
+void Chip8::OP_Annn()
+{
+    // Set I = nnn
+}
+
+void Chip8::OP_Bnnn()
+{
+    // Jump to location nnn + V0
+}
+
+void Chip8::OP_Cxkk()
+{
+    // Set Vx = random byte AND kk
+}
+
+void Chip8::OP_Dxyn()
+{
+    // Display n-byte sprite starting at memory[I] at (Vx, Vy), set VF = collision
+}
+
+void Chip8::OP_Ex9E()
+{
+    // Skip next instruction if key with the value of Vx is pressed
+}
+
+void Chip8::OP_ExA1()
+{
+    // Skip next instruction if key with the value of Vx is not pressed
+}
+
+void Chip8::OP_Fx07()
+{
+    // Set Vx = delay timer value
+}
+
+void Chip8::OP_Fx0A()
+{
+    // Wait for a key press, store the value of the key in Vx
+}
+
+void Chip8::OP_Fx15()
+{
+    // Set delay timer = Vx
+}
+
+void Chip8::OP_Fx18()
+{
+    // Set sound timer = Vx
+}
+
+void Chip8::OP_Fx1E()
+{
+    // Set I = I + Vx
+}
+
+void Chip8::OP_Fx29()
+{
+    // Set I = location of sprite for digit Vx
+}
+
+void Chip8::OP_Fx33()
+{
+    // Store BCD representation of Vx in memory locations I, I+1, and I+2
+}
+
+void Chip8::OP_Fx55()
+{
+    // Store registers V0 through Vx in memory starting at location I
+}
+
+void Chip8::OP_Fx65()
+{
+    // Read registers V0 through Vx from memory starting at location I
+}
